d194a473fd24986ad96badc7ca7a8df4426b0dcd
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-03-05 11:14:17 +0100

    Modified path

--- tests/test_integer_encode.py
+++ tests/test_integer_encode.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.dirname(os.getcwd())+'/protclass')
+sys.path.insert(0, os.path.dirname(os.getcwd())+'/ProtClass/protclass')
 print(sys.path)
 import numpy as np
 
