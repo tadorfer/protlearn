b4905b9c8d40a94ef063c61168f8c41b1da535f4
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-06 10:32:10 +0200

    adjusted test files for new input formats

--- tests/test_composition.py
+++ tests/test_composition.py
@@ -3,7 +3,6 @@ import sys
 path = os.environ.get('TRAVIS_BUILD_DIR')
 sys.path.insert(0, path+'/protlearn')
 
-from preprocessing import txt_to_df
 from feature_engineering import composition
 
 
@@ -11,27 +10,27 @@ def test_composition():
     "Test sequence compositions"
     
     # load data
-    df = txt_to_df(path+'/tests/docs/test_seq.txt', 0)
+    data = open('/tests/docs/test_seq.txt', 'r').read().splitlines()
     
     # test relative composition
-    comp_rel = composition(df, 'relative')
+    comp_rel = composition(data, 'relative')
     
     # test if frequencies add to 1
-    for i in range(df.shape[0]):
+    for i in range(len(data)):
         assert round(comp_rel.iloc[0,:].sum()) == 1
         
     # test decimal places
-    comp_dec2 = composition(df, 'relative', round_fraction=2)
+    comp_dec2 = composition(data, 'relative', round_fraction=2)
     x = str(comp_dec2['A'][0])
     assert x[::-1].find('.') == 2
-    comp_dec2 = composition(df, 'relative', round_fraction=5)
+    comp_dec2 = composition(data, 'relative', round_fraction=5)
     y = str(comp_dec2['A'][0])
     assert y[::-1].find('.') == 5
     
     # test absolute composition
-    comp_abs = composition(df, 'absolute')
+    comp_abs = composition(data, 'absolute')
     
     # test if frequences == sequence length
     all_lengths = [6, 9, 7, 6]
-    for i in range(df.shape[0]):
+    for i in range(len(data)):
         assert comp_abs.iloc[i,:].sum() == all_lengths[i]
