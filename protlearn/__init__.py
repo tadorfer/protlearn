158ebdf0fdf12c1f96bf6ed7c033bc1a5359b9a2
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-06 10:36:24 +0200

    removed old function from __init__.py

--- protlearn/__init__.py
+++ protlearn/__init__.py
@@ -1,11 +1,8 @@
-from protlearn.preprocessing import txt_to_df
 from protlearn.preprocessing import integer_encode
 
 from protlearn.feature_engineering import length
 from protlearn.feature_engineering import composition
 from protlearn.feature_engineering import aaindex1
-from protlearn.feature_engineering import aaindex2
-from protlearn.feature_engineering import aaindex3
 from protlearn.feature_engineering import ngram_composition
 from protlearn.feature_engineering import position_enrichment
 
