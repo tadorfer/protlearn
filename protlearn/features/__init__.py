e8573d8f0473f15851377c6668ad3d8b7c58d198
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-11 14:54:17 +0200

    changed test paths

--- protlearn/features/__init__.py
+++ protlearn/features/__init__.py
@@ -1,5 +1,5 @@
-from length import length
-from aac import aac
+from .length import length
+from .aac import aac
 from aaindex1 import aaindex1
 from ngram import ngram_composition
 from enrichment import position_enrichment
