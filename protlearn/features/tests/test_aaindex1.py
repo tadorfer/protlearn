ed29bdc9255b007504416d1289fc6ef09b4666b7
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-11 18:58:17 +0200

    changed test scripts from dataframe to array

--- protlearn/features/tests/test_aaindex1.py
+++ protlearn/features/tests/test_aaindex1.py
@@ -8,35 +8,35 @@ def test_aaindex1():
     data = ['AGTYLK', 'VCIMMMPFP', 'LRSAHHN', 'AQEEWD'] 
     
     # get aaindex1
-    aaind1 = aaindex1(data)
+    aaind1, desc = aaindex1(data)
     
     # test shape
     assert aaind1.shape == (4, 553)
     
     # test some indices
-    ANDN920101 = np.array([4.3, 4.40555, 4.48714, 4.46])
-    QIAN880126 = np.array([.01166, -.17111, .05857, -.04333])
-    KARS160122 = np.array([2.014, 5.48522, 2.789, 1.751])
-    np.testing.assert_equal(np.round(aaind1['ANDN920101'], 3),\
+    ANDN920101 = np.array([4.3, 4.40555, 4.48714, 4.46]) # index 0
+    QIAN880126 = np.array([.01166, -.17111, .05857, -.04333]) # index 277
+    KARS160122 = np.array([2.014, 5.48522, 2.789, 1.751]) # index -1
+    np.testing.assert_equal(np.round(aaind1[0], 3),\
                             np.round(ANDN920101, 3))
-    np.testing.assert_equal(np.round(aaind1['QIAN880126'], 3),\
+    np.testing.assert_equal(np.round(aaind1[277], 3),\
                             np.round(QIAN880126, 3))
-    np.testing.assert_equal(np.round(aaind1['KARS160122'], 3),\
+    np.testing.assert_equal(np.round(aaind1[-1], 3),\
                             np.round(KARS160122, 3))
     
     # test standardization (zscore)
-    aaind1_z = aaindex1(data, 'zscore')
+    aaind1_z, desc = aaindex1(data, 'zscore')
     # test mean = 0
     for i in range(aaind1_z.shape[0]):
-        assert abs(round(aaind1_z.iloc[:,1].mean())) == 0
+        assert abs(round(aaind1_z[:,1].mean())) == 0
     # test std --> 1
     for i in range(aaind1_z.shape[0]):
-        assert round(aaind1_z.iloc[:,i].std(), 1) ==\
-               round(aaind1_z.iloc[:,0].std(), 1)
+        assert round(aaind1_z[:,i].std(), 1) ==\
+               round(aaind1_z[:,0].std(), 1)
         
     # test standardization (minmax)
-    aaind1_mm = aaindex1(data, 'minmax')
+    aaind1_mm, desc = aaindex1(data, 'minmax')
     # test minimum and maximum
     for i in range(aaind1_mm.shape[0]):
-        assert round(aaind1_mm.iloc[:,i].min()) == 0
-        assert round(aaind1_mm.iloc[:,i].max()) == 1
+        assert round(aaind1_mm[:,i].min()) == 0
+        assert round(aaind1_mm[:,i].max()) == 1
--- protlearn/features/tests/test_composition.py
+++ protlearn/features/tests/test_composition.py
@@ -1,30 +1,22 @@
 from features import aac
 
-def test_composition():
+def test_aac():
     "Test sequence compositions"
     
     # load data
     data = ['AGTYLK', 'VCIMMMPFP', 'LRSAHHN', 'AQEEWD']
     
     # test relative composition
-    comp_rel = aac(data, 'relative')
+    comp_rel, aa = aac(data, 'relative')
     
     # test if frequencies add to 1
     for i in range(len(data)):
-        assert round(comp_rel.iloc[0,:].sum()) == 1
-        
-    # test decimal places
-    comp_dec2 = aac(data, 'relative', round_fraction=2)
-    x = str(comp_dec2['A'][0])
-    assert x[::-1].find('.') == 2
-    comp_dec2 = aac(data, 'relative', round_fraction=5)
-    y = str(comp_dec2['A'][0])
-    assert y[::-1].find('.') == 5
+        assert round(comp_rel[0,:].sum()) == 1
     
     # test absolute composition
-    comp_abs = aac(data, 'absolute')
+    comp_abs, aa = aac(data, 'absolute')
     
     # test if frequences == sequence length
     all_lengths = [6, 9, 7, 6]
     for i in range(len(data)):
-        assert comp_abs.iloc[i,:].sum() == all_lengths[i]
+        assert comp_abs[i,:].sum() == all_lengths[i]
