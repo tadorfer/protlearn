8d527ddc1ab46b34e813b3a59b81ef0d312ecce4
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-13 08:38:24 +0200

    imported correct utils function

--- protlearn/utils/tests/test_validation.py
+++ protlearn/utils/tests/test_validation.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from utils import check_input
+from utils.validation import check_input
 import pkg_resources
 
 PATH = pkg_resources.resource_filename(__name__, 'test_data/')
