a6c947d826a77b99e24d2af30c963f056caab209
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-17 13:22:30 +0200

    added test for paac.py

---
+++ protlearn/features/tests/test_paac.py
@@ -0,0 +1,39 @@
+import pytest
+import numpy as np
+from features import paac
+import pkg_resources
+
+PATH = pkg_resources.resource_filename(__name__, 'test_data/')
+
+def test_paac():
+    "Test pseudo amino acid composition"
+    
+    # load data
+    X_list = open(PATH+'multiple.txt').read().splitlines()
+    X_err = 'AGT2HT9'
+    
+    # get paac
+    paac_list, aa = paac(X_list)
+    
+    # test paac
+    np.testing.assert_almost_equal(paac_list, 
+    np.array([[1.79965836, 0.        , 0.        , 0.        , 0.        ,
+        0.        , 0.        , 0.        , 0.89982918, 1.79965836,
+        0.        , 0.        , 0.        , 0.        , 0.89982918,
+        0.        , 0.        , 0.        , 0.        , 0.89982918,
+        0.10017082],
+       [0.        , 0.92749507, 0.92749507, 0.92749507, 0.        ,
+        1.85499014, 0.        , 0.        , 0.        , 1.85499014,
+        0.        , 0.        , 1.85499014, 0.        , 0.        ,
+        0.        , 0.        , 0.        , 0.        , 0.        ,
+        0.07250493],
+       [2.77372003, 0.92457334, 1.84914669, 0.        , 0.        ,
+        0.        , 0.        , 0.        , 0.        , 0.        ,
+        0.        , 0.92457334, 0.        , 0.        , 0.92457334,
+        0.        , 0.        , 0.        , 0.        , 0.        ,
+        0.07542666]])
+    , decimal=3)
+
+    # test ValueError
+    with pytest.raises(ValueError):
+        paac_error, desc = paac(X_err)
