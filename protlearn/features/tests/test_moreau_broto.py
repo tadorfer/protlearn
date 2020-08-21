846cb308f3256eca277ab27a3322666f5420727a
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-21 11:48:04 +0200

    renamed file

--- protlearn/features/tests/test_moreau_broto.py
+++ protlearn/features/tests/test_moreau_broto.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from features import moreau-broto
+from features import moreau_broto
 import pkg_resources
 
 PATH = pkg_resources.resource_filename(__name__, 'test_data/')
