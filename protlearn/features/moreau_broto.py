cf7967d5e102c4fbca1b202fd6bd4fbc27f04181
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-21 11:21:15 +0200

    finished moreau_broto autocorrelation

---
+++ protlearn/features/moreau_broto.py
@@ -0,0 +1,82 @@
+# Author: Thomas Dorfer <thomas.a.dorfer@gmail.com>
+
+import numpy as np
+import pandas as pd
+from Bio.Alphabet import IUPAC
+from utils.validation import check_input
+import pkg_resources
+
+PATH = pkg_resources.resource_filename(__name__, 'data/')
+
+# default indices of AAIndex1 (Xiao et al., 2015)
+default = ['CIDH920105', 'BHAR880101', 'CHAM820101', 'CHAM820102',
+           'CHOC760101', 'BIGC670101', 'CHAM810101', 'DAYM780201']
+
+def moreau_broto(X, d=1, properties=default, start=1, end=None): 
+    """Compute the normalized Moreau-Broto Autocorrelation based on AAIndex1 
+       features.
+
+    Parameters
+    ----------
+
+    X : string, fasta, or a list thereof 
+    
+    properties : list
+        List of strings denoting AAIndex1 indices.
+        
+    d : int, default=1
+        Represents the lag. Must be smaller than sequence length.
+        Maximum: 30.
+    
+    start : int, default=1
+        Determines the starting point of the amino acid sequence.
+
+    end : int, default=None
+        Determines the end point of the amino acid sequence.
+
+    Returns
+    -------
+
+    arr :  ndarray of shape (n_samples, n_properties)
+
+    """
+
+    # input handling
+    X = check_input(X)
+    min_len = min([len(seq) for seq in X])
+    if d >= 30:
+        raise ValueError('Maximum lag parameter is 30!')
+    if d >= min_len:
+        raise ValueError('Lag parameter d must be smaller than sequence length!')
+    
+    # load data
+    df = pd.read_csv(PATH+'aaindex1.csv').set_index('Description')
+    df = df.reindex(sorted(df.columns), axis=1)
+    data = np.asarray(df.loc[default])
+
+    # list of amino acids (IUPAC standard)
+    amino_acids = IUPAC.IUPACProtein.letters
+    aadict = {amino_acids[i]: i for i in range(20)}
+
+    # standardization
+    for i in range(data.shape[0]):
+        data[i,:] = [(j-np.mean(data[i,:]))/np.std(data[i,:]) for j in data[i,:]]
+
+    # calculate normalized Moreau-Broto 
+    arr = np.zeros((len(X), len(default)))
+    for i, seq in enumerate(X):
+        # check that input is alphabetical
+        if str.isalpha(seq) == True:
+            pass
+        else:
+            raise ValueError('Data must be alphabetical!')
+
+        seq = seq[start-1:end] # positional information
+        for j in range(len(default)):
+            p = [data[j, aadict[aa]] for aa in seq]
+            ac = sum([(p[i]*p[i+d]) for i in range(len(seq)-d)])
+            ats = ac/(len(seq)-d) # normalizing
+
+            arr[i,j] = ats
+            
+    return arr
