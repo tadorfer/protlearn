7645d7e1b59376527159ffc73f8c4f071cadc29a
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-22 13:26:44 +0200

    imported more modules

--- protlearn/__init__.py
+++ protlearn/__init__.py
@@ -1,1 +1,9 @@
-__all__ = ['utils', 'preprocessing', 'features', 'dimreduction']
+from . import preprocessing
+from . import features
+from . import dimreduction
+
+__version__ = '1.9.1'
+
+__all__ = ['preprocessing',
+           'features',
+           'dimreduction']
