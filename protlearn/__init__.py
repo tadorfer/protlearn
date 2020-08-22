2ac3fd6290a2d7c075d82cddb54f098d61aa5403
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-22 15:11:39 +0200

    fixed import issues

--- protlearn/__init__.py
+++ protlearn/__init__.py
@@ -1,9 +1,20 @@
-from . import preprocessing
-from . import features
-from . import dimreduction
-from . import utils
+from preprocessing import encode
 
-__version__ = '1.9.1'
+from features import ngram
+from features import posrich
+from features import entropy
+from features import paac
+from features import cksaap
+from features import ctd
+from features import binary
+from features import atc
+from features import moran
+from features import geary
+from features import moreau_broto
+
+from utils.validation import check_input
+
+__version__ = '1.9.2'
 
 __all__ = ['preprocessing',
            'features',
