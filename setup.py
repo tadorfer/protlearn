3d7da161a71dc54b8ec4bba7d7ad795b47e18739
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-22 15:51:49 +0200

    prepared for pre-release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
   name = 'protlearn',       
   packages = ['protlearn'], 
   package_data={'protlearn': ['features/data/*.csv']},  
-  version = '1.9.2',      
+  version = '1.9.3',      
   license='MIT',        
   description = 'Feature engineering for protein sequences', 
   long_description_content_type='text/markdown',
@@ -19,7 +19,7 @@ setup(
   author = 'Thomas Dorfer',                   
   author_email = 'thomas.a.dorfer@gmail.com',   
   url = 'https://github.com/tadorfer/ProtLearn',   
-  download_url = 'https://github.com/tadorfer/ProtLearn/archive/v1.9.2.tar.gz',  
+  download_url = 'https://github.com/tadorfer/ProtLearn/archive/v1.9.3.tar.gz',  
   keywords = ['amino acids', 'proteins', 'peptides', 'preprocessing', 'feature engineering', 'visualization'], 
   setup_requires = ['wheel'],
   install_requires=[            
