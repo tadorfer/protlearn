e12e6a2a188a10045d4a65a25538a500376ece12
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-08-23 09:59:04 +0200

    modified init files

--- protlearn/__init__.py
+++ protlearn/__init__.py
@@ -1,3 +1,16 @@
+"""
+Protein feature extraction module for Python
+============================================
+protlearn is a Python module for the extraction of features from amino acid 
+sequences. It is comprised of three submodules - preprocessing, features, and 
+dimensionality reduction.
+
+It aims to provide simple and efficient functions to extract protein features 
+useful for downstream classification or regression tasks.
+
+See https://protlearn.readthedocs.io/en/latest/ for complete documentation.
+"""
+
 __version__ = '1.9.2'
 
 __all__ = ['preprocessing',
