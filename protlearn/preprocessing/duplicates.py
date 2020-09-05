4b8e04642b7e13ec61f02e0a3d0d2a95aff62a10
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-05 10:41:24 +0200

    implemented duplicate sequence removal

---
+++ protlearn/preprocessing/duplicates.py
@@ -0,0 +1,47 @@
+# Author: Thomas Dorfer <thomas.a.dorfer@gmail.com>
+
+from collections import Counter
+from ..utils.validation import check_input
+
+def duplicates(X, verbose=1):
+    """Remove duplicate sequences.
+
+        Parameters
+        ----------
+
+        X : string, fasta, or a list thereof 
+        
+        verbose: int, default=1
+            
+            0: no information on duplicates is printed
+            1: prints number of duplicates removed
+            2: prints duplicate sequences and number of times present
+
+        Returns
+        -------
+
+        Y :  list of length n_samples
+
+        """
+
+    # input handling
+    X = check_input(X)
+
+    # remove duplicates
+    Y = list(set(X))
+    diff = Counter(X)-Counter(Y)
+    
+    if diff:
+        if verbose == 1:
+            if len(diff) == 1:
+                print("1 duplicate has been removed.")
+            else:
+                print("{} duplicates have been removed.".format(len(diff)))
+        elif verbose == 2:
+            for entry in diff:
+                print("Removed duplicates [sequence: number of times present]:\n")
+                print("{0}: {1}".format(entry, diff[entry]+1))
+    else:
+        print("Data contains no duplicates.")
+    
+    return Y
