7b5ed623df02e2abd5199fa99344ac9abf30e8c2
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-06 20:14:42 +0200

    added t-SNE

--- protlearn/dimreduction/__init__.py
+++ protlearn/dimreduction/__init__.py
@@ -1,7 +1,9 @@
 from .correlation import correlation
 from .pca import pca
 from .rfe import rfe
+from .tsne import tsne
 
 __all__ = ['correlation',
            'pca',
-           'rfe']
+           'rfe',
+           'tsne']
