e562199bcf8521b7d3bf85bf8439118db3cf8653
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-07 11:26:37 +0200

    adjusted test data

--- protlearn/dimreduction/tests/test_rf_importance.py
+++ protlearn/dimreduction/tests/test_rf_importance.py
@@ -11,19 +11,20 @@ def test_rf_importance():
     
     # load data
     X = np.load(PATH+'features_largeN.npy')
+    y = np.load(PATH+'features_largeN_labels.npy')
 
     # compute RF-based feature importances
     X_reduced = rf_importance(X, y, top=5)
 
     # test array contents
     np.testing.assert_almost_equal(X_reduced[0,:], np.array([
-        0.37577778, 0.28177778, 8.36255556, 2.22222222, 6.64444444]), decimal=3)
+        1.38777778,  4.33666667,  1.12444444, 43.64444444,  0.90533333]), decimal=3)
 
     np.testing.assert_almost_equal(X_reduced[300,:], np.array([
-        0.77022222,   0.31077778,   8.37488889, -43., 5.97777778]), decimal=3)
+        1.11666667,  4.61333333,  1.17111111, 31.83333333,  0.90266667]), decimal=3)
 
     np.testing.assert_almost_equal(X_reduced[-1,:], np.array([
-        0.60611111,  0.365,  8.36233333, -0.44444444,  6.73333333]), decimal=3)
+        1.25888889,  6.01      ,  1.08      , 22.86666667,  0.92633333]), decimal=3)
 
     # test array shape
     X_reduced.shape == (700, 5)
--- protlearn/dimreduction/tests/test_xgb_importance.py
+++ protlearn/dimreduction/tests/test_xgb_importance.py
@@ -11,19 +11,20 @@ def test_xgb_importance():
     
     # load data
     X = np.load(PATH+'features_largeN.npy')
+    y = np.load(PATH+'features_largeN_labels.npy')
 
     # compute XGB-based feature importances
-    X_reduced = rf_importance(X, y, top=5)
+    X_reduced = xgb_importance(X, y, top=5)
 
     # test array contents
     np.testing.assert_almost_equal(X_reduced[0,:], np.array([
-        0.11111111, 0.        , 0.        , 0.        , 0.]), decimal=3)
+        -0.01,  1.14444444,  4.73777778,  0.16666667, -0.67777778]), decimal=3)
 
     np.testing.assert_almost_equal(X_reduced[300,:], np.array([
-        0.11111111, 0.        , 0.        , 0.        , 0.]), decimal=3)
+        0.82777778,  1.03333333,  6.01888889,  0.07333333, -0.57888889]), decimal=3)
 
     np.testing.assert_almost_equal(X_reduced[-1,:], np.array([
-        0.        , 0.        , 0.22222222, 0.        , 0.11111111]), decimal=3)
+        -0.52222222,  0.95555556,  6.02444444,  0.82555556, -0.97555556]), decimal=3)
 
     # test array shape
     X_reduced.shape == (700, 5)
