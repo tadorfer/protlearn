5ea46aedcd43c8c65863ed03c598bb7a6ca9f6ea
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-07 11:12:36 +0200

    added rf_importance and xgb_importance

--- protlearn/dimreduction/__init__.py
+++ protlearn/dimreduction/__init__.py
@@ -2,8 +2,12 @@ from .correlation import correlation
 from .pca import pca
 from .rfe import rfe
 from .tsne import tsne
+from .rf_importance import rf_importance
+from .xgb_importance import xgb_importance
 
 __all__ = ['correlation',
            'pca',
            'rfe',
-           'tsne']
+           'tsne',
+           'rf_importance',
+           'xgb_importance']
