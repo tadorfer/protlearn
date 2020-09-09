06d2baee3a16ed71dbfa39f628f0512be09f25fb
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-09 10:20:51 +0200

    added chi_squared and f_test for feature selection

--- protlearn/dimreduction/__init__.py
+++ protlearn/dimreduction/__init__.py
@@ -4,10 +4,14 @@ from .rfe import rfe
 from .tsne import tsne
 from .rf_importance import rf_importance
 from .xgb_importance import xgb_importance
+from .chi_squared import chi_squared
+from .f_test import f_test
 
 __all__ = ['correlation',
            'pca',
            'rfe',
            'tsne',
            'rf_importance',
-           'xgb_importance']
+           'xgb_importance',
+           'chi_squared',
+           'f_test']
