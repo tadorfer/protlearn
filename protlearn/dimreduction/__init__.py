5a8c5965261911bfa7185b37f67e0cc607390b99
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-12 14:50:42 +0200

    added tree-based feature importances

--- protlearn/dimreduction/__init__.py
+++ protlearn/dimreduction/__init__.py
@@ -4,9 +4,7 @@ from .rfe import rfe
 from .tsne import tsne
 from .rf_importance import rf_importance
 from .xgb_importance import xgb_importance
-from .chi_squared import chi_squared
-from .f_test import f_test
-from .mutual_information import mutual_information
+from .tree_importance import tree_importance
 from .univariate_filter import univariate_filter
 from .lasso import lasso
 from .sequential import sequential
@@ -17,9 +15,7 @@ __all__ = ['correlation',
            'tsne',
            'rf_importance',
            'xgb_importance',
-           'chi_squared',
-           'f_test',
-           'mutual_information',
+           'tree_importance',
            'univariate_filter',
            'lasso',
            'sequential']
