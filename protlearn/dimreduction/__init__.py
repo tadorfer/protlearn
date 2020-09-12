3d80af3fd11bd417e183f385355c1b5b566ac4fa
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-12 16:23:53 +0200

    merged RF and XGB into tree_importance

--- protlearn/dimreduction/__init__.py
+++ protlearn/dimreduction/__init__.py
@@ -2,8 +2,6 @@ from .correlation import correlation
 from .pca import pca
 from .rfe import rfe
 from .tsne import tsne
-from .rf_importance import rf_importance
-from .xgb_importance import xgb_importance
 from .tree_importance import tree_importance
 from .univariate_filter import univariate_filter
 from .lasso import lasso
@@ -13,8 +11,6 @@ __all__ = ['correlation',
            'pca',
            'rfe',
            'tsne',
-           'rf_importance',
-           'xgb_importance',
            'tree_importance',
            'univariate_filter',
            'lasso',
