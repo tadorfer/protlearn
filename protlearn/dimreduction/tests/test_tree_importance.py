02a9e8f10134fbc05b9ac354e11d0cebfbad14bc
Thomas Dorfer <thomas.a.dorfer@gmail.com>
2020-09-12 16:23:38 +0200

    testing custom classifier

--- protlearn/dimreduction/tests/test_tree_importance.py
+++ protlearn/dimreduction/tests/test_tree_importance.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from xgboost import XGBClassifier
 from ..tree_importance import tree_importance
 
 import pkg_resources
@@ -25,6 +26,19 @@ def test_tree_importance():
         0.82777778,  1.03333333,  6.01888889,  0.07333333, -0.57888889]), decimal=3)
 
     np.testing.assert_almost_equal(X_xgb[-1,:], np.array([
+        -0.52222222,  0.95555556,  6.02444444,  0.82555556, -0.97555556]), decimal=3)
+
+    # test customized classifier
+    clf = XGBClassifier()
+    X_clf = tree_importance(X, y, clf=clf, top=5)
+
+    np.testing.assert_almost_equal(X_clf[0,:], np.array([
+        -0.01,  1.14444444,  4.73777778,  0.16666667, -0.67777778]), decimal=3)
+
+    np.testing.assert_almost_equal(X_clf[300,:], np.array([
+        0.82777778,  1.03333333,  6.01888889,  0.07333333, -0.57888889]), decimal=3)
+
+    np.testing.assert_almost_equal(X_clf[-1,:], np.array([
         -0.52222222,  0.95555556,  6.02444444,  0.82555556, -0.97555556]), decimal=3)
 
     # test array shape
